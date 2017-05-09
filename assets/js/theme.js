aee9736276de80e5e8cc156562b976f1cec226ec
James King <hello@chromaticaldesign.com>
2017-05-09 16:53:03 -0700

    Add tracking and async scripts

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+  Handlebars.registerHelper('gt', function(val1, val2, options) {
+    if (val1 >= val2){
+      return options.fn(this)
+    }
+    return options.inverse(this)
+  })
+
+  Handlebars.registerHelper('lt', function(val1, val2, options) {
+    if (val1 < val2){
+      return options.fn(this)
+    }
+    return options.inverse(this)
+  })
   var ratingTemplate = Handlebars.compile($('#ratingTemplate').html())
 
   var swiper = new Swiper('.swiper-container', {
